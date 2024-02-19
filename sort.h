adeec22def3067062908c6f5e84738e41fd80c83
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-20 00:27:15 +0100

    selection sort function

--- sort.h
+++ sort.h
@@ -4,12 +4,14 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 void print_array(const int *array, size_t size);
 /*void print_list(const listint_t *list);*/
 void bubble_sort(int *array, size_t size);
 void swap(int *xp, int *yp);
-
+/*void insertion_sort_list(listint_t **list);*/
+void selection_sort(int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
